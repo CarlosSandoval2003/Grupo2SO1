0e0f1ac0475a6444a18188dfe4224da6dc25afe1
Nelson Pineda <npinedac3@miumge.edu.gt>
2023-09-12 23:23:27 -0600

    agregando base de librería

--- Administrador de Trareas/MainPage.xaml.cs
+++ Administrador de Trareas/MainPage.xaml.cs
@@ -12,6 +12,13 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using System.Threading.Tasks;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
