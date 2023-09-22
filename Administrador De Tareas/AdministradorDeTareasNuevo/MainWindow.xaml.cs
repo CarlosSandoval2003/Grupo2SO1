96c361a5697bfd8325906babec945ccc53591797
CarlosSandoval2003 <jawicho@outlook.com>
2023-09-22 07:21:54 -0600

    Se crea el nuevo proyecto para el administrador de tareas

---
+++ Administrador De Tareas/AdministradorDeTareasNuevo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Administrador
+{
+    public partial class MainWindow : Window
+    {
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           
+
+}
+    }
+}
+
