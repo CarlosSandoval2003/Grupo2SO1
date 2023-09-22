e1ceacc2eaf433262f9be7d67007b7f8ab95ba9f
CarlosSandoval2003 <jawicho@outlook.com>
2023-09-22 07:24:26 -0600

    Se crean las funciones y clases basicas para el funcionamiento

--- Administrador De Tareas/AdministradorDeTareasNuevo/MainWindow.xaml.cs
+++ Administrador De Tareas/AdministradorDeTareasNuevo/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ namespace Administrador
 {
     public partial class MainWindow : Window
     {
+        private List<ProcessInfo> processes = new List<ProcessInfo>();
+        private List<int> selectedProcessIds = new List<int>();
+        private PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
 
 
 
@@ -19,9 +22,55 @@ namespace Administrador
         {
             InitializeComponent();
 
-           
+            // Configurar el DataGrid
+            processDataGrid.AutoGenerateColumns = true;
+            processDataGrid.SelectionMode = DataGridSelectionMode.Extended;
 
-}
+            // Actualizar la lista de procesos al inicio
+            UpdateProcessList();
+
+            // Iniciar un temporizador para actualizar la lista de procesos cada segundo
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += (s, e) => UpdateProcessList();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Start();
+        }
+
+        private void UpdateProcessList()
+        {
+
+        }
+
+
+        private void TerminateSelectedProcesses(IEnumerable<int> processIds)
+        {
+        }
+
+        private void TerminateMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TerminateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private IEnumerable<int> GetSelectedProcessIds()
+        {
+            var selectedProcesses = processDataGrid.SelectedItems.Cast<ProcessInfo>();
+            return selectedProcesses.Select(p => p.PID);
+        }
     }
+
+    public class ProcessInfo
+    {
+        public string Nombre { get; set; }
+        public int PID { get; set; }
+        public double CPU { get; set; }
+        public long Memoria { get; set; }
+
+    }
+
 }
 
